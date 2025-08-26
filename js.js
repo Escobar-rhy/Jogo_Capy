2663a05610bcb07f4dfab4e67d1597ad1f74dfd6
Escobar-rhy <rhayana764@gmail.com>
2025-08-26 16:43:27 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,11 @@
+const bntPlay = document.getElementById('bntPlay');
+
+bntPlay.addEventListener('click', function(){
+    window.location.href = 'JogoCapyTela.html'
+});
+
+const imgAjuda = document.getElementById('imgAjuda');
+
+imgAjuda.addEventListener('click', function(){
+    alert("Ajude a capivara a atravessar o rio...")
+});
